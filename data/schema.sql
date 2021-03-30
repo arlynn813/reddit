0e788f731c224e416cc7b49fe516c2d04a7c31f9
arlynn813 <lynn12@purdue.edu>
2021-03-30 17:21:26 -0400

    first schema design

---
+++ data/schema.sql
@@ -0,0 +1,27 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE user(
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    id INTEGER PRIMARY KEY,  -- will be hashed from username
+    UNIQUE(username, email)
+);
+
+CREATE TABLE post(
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL,  -- users post articles
+    FOREIGN KEY(user_id) REFERENCES user(id)
+);
+
+CREATE TABLE vote(
+    like INTEGER NOT NULL,  -- 0 for downvote, 1 for upvote
+    user_id INTEGER NOT NULL,  -- user can only vote once per post
+    post_id INTEGER NOT NULL,  -- vote is placed on post
+    FOREIGN KEY(user_id) REFERENCES user(id),
+    FOREIGN KEY(post_id) REFERENCES post(id)
+);
