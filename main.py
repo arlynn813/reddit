94076da70eb6ce31bd5c84f9b552b5f3b3865f40
arlynn813 <lynn12@purdue.edu>
2021-03-31 18:17:24 -0400

    register api

---
+++ main.py
@@ -0,0 +1,26 @@
+import hashlib
+from flask import Flask, redirect, request, render_template, url_for
+from user import User
+
+app = Flask(__name__, static_folder='styles')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        user_id = hashlib.sha1(request.form['username'].encode('utf-8')).hexdigest()
+        user = User.get(user_id)
+        if not user:
+            user = User.create(request.form['first_name'], request.form['last_name'], request.form['username'],
+                               request.form['email'])
+        return redirect(url_for('feed', user_id=user.id))
+    return render_template('register.html')
+
+
+@app.route('/<user_id>/feed', methods=['GET'])
+def feed(user_id):
+    return f'Render feed for {user_id}...'
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8000, debug=True)
