02ce78a090c4bae4c95a5bc0048a86f89b7c2aa6
arlynn813 <lynn12@purdue.edu>
2021-04-01 00:52:15 -0400

    vote orm

--- post.py
+++ post.py
@@ -6,8 +6,9 @@ class Post:
     @classmethod
     def objects(cls, user=None, exclude=False):
         entries = cls.__get(user=user, exclude=exclude)
-        return [Post(entry['title'], entry['content'], entry['created_at'], entry['user_id'], id_=entry['id']
-                     ) for entry in entries]
+        posts = [Post(entry['title'], entry['content'], entry['created_at'], entry['user_id'], id_=entry['id']
+                      ) for entry in entries]
+        return sorted(posts, key=lambda x:x.created_at, reverse=True)
 
     @classmethod
     def get(cls, id_):
@@ -65,7 +66,56 @@ class Post:
         return f'DELETE FROM post WHERE id={self.id};'
 
 
+class Vote:
+    @classmethod
+    def objects(cls, post=None):
+        entries = cls.__get(post=post)
+        return [Vote(entry['value'], entry['user_id'], entry['post_id'], id_=entry['id']) for entry in entries]
+
+    @classmethod
+    def get(cls, id_):
+        try:
+            entry = cls.__get(id_=id_)[0]
+        except IndexError:
+            return None
+        return Vote(entry['value'], entry['user_id'], entry['post_id'], id_=entry['id'])
+
+    @classmethod
+    def create(cls, post, value):
+        vote = Vote(value, post.user.id, post.id)
+        vote.__create()
+        return vote  # note: this object does not contain the sql auto generated primary key...
+
+    def delete(self):
+        self.__delete()
+
+    def __init__(self, value, user_id, post_id, id_=None):
+        self.value = value
+        self.user_id = user_id
+        self.post_id = post_id
+        self.id = id_
+
+    # SQL methods
+    @classmethod
+    @connection_required()
+    def __get(cls, id_=None, post=None):
+        if id_:
+            return f'SELECT * FROM vote WHERE id={id_};'
+        elif post:
+            return f'SELECT * FROM vote WHERE post_id={post.id};'
+        return 'SELECT * FROM vote;'
+
+    @connection_required(commit=True)
+    def __create(self):
+        return f'INSERT INTO vote (value, user_id, post_id) VALUES({self.value}, "{self.user_id}", {self.post_id});'
+
+    @connection_required(commit=True)
+    def __delete(self):
+        return f'DELETE FROM vote WHERE id={self.id};'
+
+
 if __name__ == '__main__':
+    """
     from user import User
     user1 = User.create('Andrew', 'Lynn', 'arlynn813', 'arlynn813@gmail.com')
     user2 = User.create('First', 'Last', 'test_username', 'test@test.com')
@@ -90,3 +140,12 @@ if __name__ == '__main__':
     for p in all_posts:
         p.delete()
     print(Post.objects())
+    """
+    from user import User
+    from post import Post, Vote
+
+    vote = Vote.get(1)
+    print('VOTE: ', vote)
+    vote.delete()
+    print(Vote.objects())
+
