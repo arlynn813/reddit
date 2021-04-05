684ab5ce213515d9412c77c9a892798a6792588d
arlynn813 <lynn12@purdue.edu>
2021-04-05 19:51:34 -0400

    post delete - user confirmation

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
     $('button[name="delete"]').click(function() {
-        let post_id = $(this).val();
-        let user_id = $('#user_id').attr('content');
-        $.ajax({
-            url: '/' + user_id + '/profile/' + post_id + '/delete',
-            type: 'GET',
-            success: function() {
-                $('#' + post_id).remove();
-            }
-        });
+        let confirmation = confirm('Are you sure you want to delete this post?');
+        if (confirmation) {
+            let post_id = $(this).val();
+            let user_id = $('#user_id').attr('content');
+            $.ajax({
+                url: '/' + user_id + '/profile/' + post_id + '/delete',
+                type: 'GET',
+                success: function () {
+                    $('#' + post_id).remove();
+                }
+            });
+        }
     })
 });
