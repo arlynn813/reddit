7d8abda5b0a0b0377f5b7285ea5138a36de25e6e
arlynn813 <lynn12@purdue.edu>
2021-04-26 19:56:56 -0400

    fixed pagination

--- scripts/pagination.js
+++ scripts/pagination.js
@@ -3,12 +3,11 @@ $(document).ready(function() {
     let page_display = $('#page_display');
     let previous_button = $('#previous');
     let next_button = $('#next');
+    let page_idx = 0;
+    let posts = $('.feed-post');
     paginate();
 
     function paginate() {
-        let page_idx = parseInt(page.attr('content'));
-        let posts = $('.feed-post');
-
         posts.each(function (i) {
             if (i >= 5 * page_idx && i < 5 * (page_idx + 1)) {
                 $(this).css('display', 'inline');
@@ -16,28 +15,21 @@ $(document).ready(function() {
                 $(this).css('display', 'none');
             }
         });
-
-        previous_button.prop('disabled', false);
-        next_button.prop('disabled', false);
-        if (page_idx <= 0) {
-            previous_button.prop('disabled', true);
-        }
-        if (page_idx * 5 >= posts.length - 1) {
-            next_button.prop('disabled', true);
-        }
     }
 
     previous_button.click(function() {
-        let page_idx = parseInt(page.attr('content'));
-        page.attr('content', (page_idx - 1).toString());
-        page_display.text(page_idx);
-        paginate();
+        if (page_idx > 0) {
+            page_idx--;
+            page_display.text(page_idx + 1);
+            paginate();
+        }
     });
 
     next_button.click(function () {
-        let page_idx = parseInt(page.attr('content'));
-        page.attr('content', (page_idx + 1).toString());
-        page_display.text(page_idx + 2);
-        paginate();
+        if (page_idx * 5 < posts.length - 1) {
+            page_idx++;
+            page_display.text(page_idx + 1);
+            paginate();
+        }
     });
 });
